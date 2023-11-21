57a26c0d7615e517d2a2989ba987c6e0ba4d5e0a
EGYPT <nadiranono183@gmail.com>
2023-11-21 20:26:22 +0000

    Displayed title in console log

--- index.js
+++ index.js
@@ -86,3 +86,8 @@ var finances = [
   ['Jan-2017', 138230],
   ['Feb-2017', 671099],
 ];
+
+// The total number of months included in the dataset.
+ console.log("Financial Analysis")
+ console.log("----------------")
+ 
